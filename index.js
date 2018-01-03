79f082846d33fe8379d27b9dabe2b03030eae7e3
Sambulo Senda <sambulosendas@gmail.com>
2018-01-03 22:16:48 +0000

    existinguser

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+
+require('./models/User');
 require('./services/passport');
-require('./models/User')
+
 mongoose.connect(keys.mongoURI);
 
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,10 @@
 const passport= require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose')
 const keys = require('../config/keys');
+
+
+const User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy(
@@ -10,9 +14,17 @@ passport.use(
         callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
-            console.log('access token', accessToken);
-            console.log('refresh token', refreshToken);
-            console.log('profile:', profile);
+            //returns a promise
+            User.findOne({ googleId: profile.id})
+            .then((existingUser) =>{
+                if(existingUser){
+                    //we already have a record with given profile ID
+                }
+                else{
+                    new User({ googleId: profile.id }).save();
+
+                }
+            })
         }
     )
 );
