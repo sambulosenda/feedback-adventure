30c211bfee530fc08e9cba18ff847786aab8046d
Sambulo Senda <sambulosendas@gmail.com>
2018-01-05 06:54:37 +0000

    code all night

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+    <div>
+    hie there
+    </div>
+    );
+};
+
+export default App;
--- services/passport.js
+++ services/passport.js
@@ -23,20 +23,16 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
         },
-        (accessToken, refreshToken, profile, done) => {
-            //returns a promise
-            User.findOne({ googleId: profile.id})
-            .then((existingUser) =>{
-                if(existingUser){
-                    //we already have a record with given profile ID
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id})
+               
+            if(existingUser){
                 done(null, existingUser);
-                }
-                else{
-                    new User({ googleId: profile.id })
-                    .save()
-                    .then(user => done(null, user));
-                }
-            });
-        }
+            }
+
+            const user =  await  new User({ googleId: profile.id }).save();
+                 done(null, user)
+            }
+        
     )
 );
