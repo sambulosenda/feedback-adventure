86211810c7b50555381eae239fc80145e25a4cbf
Sambulo Senda <sambulosendas@gmail.com>
2018-01-06 11:18:12 +0000

    tired coded all night

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,12 @@ import React from 'react';
 import ReactDOM from'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
+
+import reduxThunk from 'redux-thunk';
 
 const store = createStore(reducers, {}, applyMiddleware());
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,6 +15,7 @@ app.get('/api/logout', (req, res )=> {
     res.send(req.user);
 
 });
+
 app.get('/api/current_user', (req, res) => {
     res.send(req.user);
 });
